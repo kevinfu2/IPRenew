b9965a363a8a6dfed0507d4e7983183505400094
unain <fukangnow@gmail.com>
2016-06-14 00:18:22 +0800

    first commit

---
+++ IPRenew/Mercury.cs
@@ -0,0 +1,59 @@
+﻿using HTMLFetchAndParse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace IPRenew
+{
+    public class Mercury
+    {
+        HTMLFetcher Fetcher = new HTMLFetcher();
+
+        public void RenewIP()
+        {
+            //xb2g56hc9TefbwK
+            //SecurityEncode(password, "RDpbLfCPsJZ7fiv", "yLwVl0zKqws7LgKPRQ84Mdt708T1qQ3Ha7xv3H7NyU84p21BriUWBU43odz3iP4rBL3cD02KZciXTysVXiV8ngg6vL48rPJyAUw0HurW20xqxv9aYb4M9wK1Ae0wlro510qXeU07kV57fQMc8L6aLgMLwygtc0F10a0Dg70TOoouyFhdysuRMO51yY5ZlOZZLEal1h0t9YQW0Ko7oBwmCAHoic4HYbUyVeU3sfQ1xtXcPcf1aT303wAQhv66qzW");
+            
+            string content = Fetcher.PostPage("http://172.16.2.1/?code=2&asyn=1", "");
+            
+            var lines = content.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            var id = SecurityEncode(lines[3], "xb2g56hc9TefbwK", lines[4]);
+
+            content = Fetcher.PostPage("http://172.16.2.1/?code=0&asyn=0&id="+HttpUtility.UrlEncode(id), "wan -linkDown");
+
+        }
+        public string SecurityEncode(string a, string b, string c)
+        {
+            StringBuilder d = new StringBuilder();
+            var f = a.Length;
+            var h = b.Length;
+            var m = c.Length;
+            var e = f > h ? f : h; //密码长度
+            for (var g = 0; g < e; g++)
+            {
+                var l = 187;
+                var k = 187;
+                if (g >= f)
+                {
+                    l = (int)b[g];
+                }
+                else
+                {
+                    if (g >= h)
+                    {
+                        k = (int)a[g];
+                    }
+                    else
+                    {
+                        k = (int)a[g];
+                        l = (int)b[g];
+                    }
+                }
+                d.Append(c[(k ^ l) % m]);
+            }
+            return d.ToString();
+        }
+    }
+}
---
+++ IPRenew/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;  
+
+namespace IPRenew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            if (p.IsOnline())
+                Console.WriteLine(p.GetPublicIP());
+            else
+                Console.WriteLine("Offline");
+            new Mercury().RenewIP();
+            while (!p.IsOnline())
+            {
+                Console.WriteLine("OFFLINE");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine(p.GetPublicIP());
+        }
+        public string GetPublicIP()
+        {
+            String direction = "";
+            WebRequest request = WebRequest.Create("http://bot.whatismyipaddress.com/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                direction = stream.ReadToEnd();
+            }
+            return direction;
+        }
+
+        public bool IsOnline()
+        {
+            string ipStr = "113.207.34.20";
+            Ping pingSender = new Ping();
+            //Ping 选项设置  
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            //测试数据  
+            string data = "a";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            //设置超时时间  
+            int timeout = 120;
+            //调用同步 send 方法发送数据,将返回结果保存至PingReply实例  
+            PingReply reply = pingSender.Send(ipStr, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
